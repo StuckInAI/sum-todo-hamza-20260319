6ea8857606464047925656ce8470f166a4b7ea60
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-19 22:16:36 +0500

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['typeorm', 'sqlite3'],
+  },
+}
+
+module.exports = nextConfig
